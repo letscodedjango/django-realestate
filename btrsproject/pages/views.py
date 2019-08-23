4715c39ebf24d5d7743665c511e06878862c01ba
Rajeshwar Sharma <rajeshwar.s@zymr.com>
2019-08-23 17:44:57 +0530

    Added static files to project

--- btrsproject/pages/views.py
+++ btrsproject/pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
 # lets define index function here 
+
 
 def index(request):
     return HttpResponse("<h1>Hey !! Its time for real estate</h1>")
@@ -10,3 +12,12 @@ def index(request):
 ## Now instead of HttpResponse we gonna render template
 def about(request):
     return HttpResponse("<h1>Hey !! Its time for real estate</h1>")
+
+
+# generally we dont use Httpresponse since the html might be big and hence we always use the concept of html template
+def index_template(request):
+    return render(request, 'pages/index.html')
+
+## Now instead of HttpResponse we gonna render template
+def about_template(request):
+    return render(request, 'pages/about.html')
